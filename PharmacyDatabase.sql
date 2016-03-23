1e53e71fa7ff4c3123d04b32ecaf53fb61adfbcf
Wan <whirligig@hotmail.com>
2016-03-22 17:15:40 -0700

    Update PharmacyDatabase.sql
    My git crashed, so I post this manually. This version works on my side quite well.

--- PharmacyDatabase.sql
+++ PharmacyDatabase.sql
@@ -1,53 +1,54 @@
+drop table Purchase;
+drop table Inventory;
+drop table Prescribed;
+drop table Pharmacist;
 drop table Patient;
 drop table Doctor;
 drop table Drug;
 drop table Pharmacy;
-drop table Pharmacist;
-drop table Prescribed;
-drop table Inventory;
-drop table Purchase;
 
-CREATE TABLE Patient (
-	patientID INTEGER,
-	doctorID INTEGER,
-	name CHAR(20),
-	illness CHAR(20),
-	address CHAR(30),
-	PRIMARY KEY (patientID, doctorID),
-	FOREIGN KEY (doctorID) REFERENCES Doctor);
-  
-grant select on Patient to public;
- 
+
 CREATE TABLE Doctor(
-	doctorID INTEGER,
-	name CHAR(20),
-	credentials CHAR(30),
-	address CHAR(30),
-	phoneNumber CHAR(10),
+	doctorID INTEGER NOT NULL,
+	name CHAR(20) NULL,
+	credentials CHAR(30) NULL,
+	address CHAR(30) NULL,
+	phoneNumber CHAR(10) NULL,
 	PRIMARY KEY (doctorID));
 	
 grant select on Doctor to public;
  
+ CREATE TABLE Patient (
+	patientID INTEGER NOT NULL,
+	doctorID INTEGER NOT NULL,
+	name CHAR(20) NULL,
+	illness CHAR(20) NULL,
+	address CHAR(30) NULL,
+	primary key (patientID),
+	FOREIGN KEY (doctorID) REFERENCES Doctor);
+  
+grant select on Patient to public;
+ 
 CREATE TABLE Drug(
-	drugID INTEGER,
-	name CHAR(30),
-	info CHAR(30),
-	dosage CHAR(30),
+	drugID INTEGER NOT NULL,
+	name CHAR(30) NULL,
+	info CHAR(30) NULL,
+	dosage CHAR(30) NULL,
 	PRIMARY KEY (drugID));
   
 grant select on Drug to public;
  
 CREATE TABLE Pharmacy(
-	pharmacyID INTEGER,
-	name CHAR(20),
-	address CHAR(20),
+	pharmacyID INTEGER NOT NULL,
+	name CHAR(20) NULL,
+	address CHAR(20) NULL,
 	PRIMARY KEY (pharmacyID));
   
 grant select on Pharmacy to public;
  
 CREATE TABLE Pharmacist(
-	employeeID INTEGER,
-	name CHAR(20),
+	employeeID INTEGER NOT NULL,
+	name CHAR(20) NULL,
 	pharmacyID INTEGER NOT NULL,
 	PRIMARY KEY (employeeID),
 	FOREIGN KEY (pharmacyID) REFERENCES Pharmacy);
@@ -55,11 +56,11 @@ CREATE TABLE Pharmacist(
 grant select on Pharmacist to public;
  
 CREATE TABLE Prescribed(
-	patientID INTEGER,
-	doctorID INTEGER,
-	drugID INTEGER,
-	isRenewable BIT,
-	datePrescribed DATE,
+	patientID INTEGER NOT NULL,
+	doctorID INTEGER NOT NULL,
+	drugID INTEGER NOT NULL,
+	isRenewable CHAR NULL,
+	datePrescribed DATE NULL,
 	PRIMARY KEY (patientID, doctorID, drugID),
 	FOREIGN KEY (patientID) REFERENCES Patient,
 	FOREIGN KEY (doctorID) REFERENCES Doctor,
@@ -68,9 +69,9 @@ CREATE TABLE Prescribed(
 grant select on Prescribed to public;
  
 CREATE TABLE Inventory(
-	drugID INTEGER,
-	pharmacyID INTEGER,
-	stock INTEGER,
+	drugID INTEGER NOT NULL,
+	pharmacyID INTEGER NOT NULL,
+	stock INTEGER NULL,
 	PRIMARY KEY (drugID, pharmacyID),
 	FOREIGN KEY (drugID) REFERENCES Drug,
 	FOREIGN KEY (pharmacyID) REFERENCES Pharmacy);
@@ -78,12 +79,12 @@ CREATE TABLE Inventory(
 grant select on Inventory to public;
  
 CREATE TABLE Purchase(
-	receiptNumber INTEGER,
+	receiptNumber INTEGER NOT NULL,
 	patientID INTEGER NOT NULL,
 	drugID INTEGER NOT NULL,
 	pharmacyID INTEGER NOT NULL,
-	price REAL,
-	datePurchased DATE,
+	price REAL NULL,
+	datePurchased DATE NULL,
 	PRIMARY KEY (receiptNumber),
 	FOREIGN KEY (patientID) REFERENCES Patient,
 	FOREIGN KEY (drugID) REFERENCES Drug,
